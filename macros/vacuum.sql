241aef239b6315a71e48421286de02b3a8287422
Drew Banin <drew@fishtownanalytics.com>
2017-08-30 13:35:27 -0400

    wip

---
+++ macros/vacuum.sql
@@ -0,0 +1,48 @@
+
+{#
+
+    Spec: http://docs.aws.amazon.com/redshift/latest/dg/r_VACUUM_command.html
+
+    VACUUM [ FULL | SORT ONLY | DELETE ONLY | REINDEX ]
+    [ [ table_name ] [ TO threshold PERCENT ] ]
+
+#}
+
+{% macro vacuum_all(type, to) -%}
+
+    vacuum {{ type }}
+
+{%- endmacro %}
+
+
+{# -- vacuum reindex does not work with 'to threshold percent' syntax #}
+{% macro vacuum_reindex(table_ref) -%}
+
+    vacuum reindex {{ table_ref }}
+
+{%- endmacro %}
+
+
+{% macro vacuum_table(table_ref, type, to) -%}
+
+    vacuum {{ type }} {{ table_ref }} to {{ to }} percent
+
+{%- endmacro %}
+
+
+{#
+    table_ref: ref, or schema.table qualified name of table
+    type: full | sort only | delete only | reindex
+    to: threshold for the vacuum command. In range [0, 100]
+#}
+{% macro vacuum(table_ref=none, type='full', to=95) -%}
+
+    {% if table_ref is none -%}
+        {{ vacuum_all(type, to) }}
+    {%- elif type == 'reindex' -%}
+        {{ vacuum_reindex(table_ref) }}
+    {%- else -%}
+        {{ vacuum_table(table_ref, type, to) }}
+    {%- endif -%}
+
+{%- endmacro %}
