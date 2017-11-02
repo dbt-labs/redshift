39b6f114989c92ce8589952b85719c51bb33f2ae
Drew Banin <drew@fishtownanalytics.com>
2017-11-01 21:31:47 -0400

    add description column to tables model

--- models/views/introspection/redshift_tables.sql
+++ models/views/introspection/redshift_tables.sql
@@ -1,8 +1,18 @@
-select
-    table_schema as schemaname,
-    table_name as tablename
 
-from information_schema.tables
-where table_type = 'BASE TABLE'
-  and schemaname not ilike 'pg_%'
+select
+    n.nspname AS schemaname,
+    c.relname AS tablename,
+    d.description,
+    case
+        when c.relkind = 'v' then 'view'
+        when c.relkind = 'r' then 'table'
+        else null
+    end as relation_type
+
+from pg_catalog.pg_namespace n
+join pg_catalog.pg_class c on n.oid = c.relnamespace
+left outer join pg_description d ON (d.objoid = c.oid AND d.objsubid = 0)
+
+where schemaname not like 'pg_%'
   and schemaname != 'information_schema'
+  and relkind in ('v', 'r')
