2da574ec516e46585715e83f06422949bb4c1d4e
Brian Abelson <brianabelson@gmail.com>
2017-11-03 19:05:50 +0100

    modifications to unload_table

---
+++ macros/unload.sql
@@ -0,0 +1,78 @@
+-- Redshift UNLOAD grammar (see: http://docs.aws.amazon.com/redshift/latest/dg/r_UNLOAD.html)
+{#
+UNLOAD ('select-statement')
+TO 's3://object-path/name-prefix'
+authorization
+[ option [ ... ] ]
+
+where option is
+
+{ MANIFEST
+| DELIMITER [ AS ] 'delimiter-char'
+| FIXEDWIDTH [ AS ] 'fixedwidth-spec' }
+| ENCRYPTED
+| BZIP2
+| GZIP
+| ADDQUOTES
+| NULL [ AS ] 'null-string'
+| ESCAPE
+| ALLOWOVERWRITE
+| PARALLEL [ { ON | TRUE } | { OFF | FALSE } ]
+[ MAXFILESIZE [AS] max-size [ MB | GB ] ]
+
+#}
+-- Unloads a Redshift query to S3
+{% macro unload_table(table,
+                s3_path,
+                iam_role=None,
+                aws_key=None,
+                aws_secret=None,
+                manifest=False,
+                delimiter=",",
+                null_as="",
+                max_file_size='6 GB',
+                escape=True,
+                compression=None,
+                add_quotes=False,
+                encrypted=False,
+                overwrite=False,
+                parallel=False) %}
+
+  -- compile UNLOAD statement
+  UNLOAD ('SELECT * FROM {{ table }}')
+  TO '{{ s3_path }}'
+  {% if iam_role %}
+  IAM_ROLE '{{ iam_role }}'
+  {% elif aws_key and aws_secret %}
+  ACCESS_KEY_ID '{{ aws_key }}'
+  SECRET_ACCESS_KEY '{{ aws_secret }}'
+  {% else %}
+  -- Raise an error if authorization args are not present
+  {{ exceptions.raise_compiler_error("You must provide AWS authorization parameters via 'iam_role' or 'aws_key' and 'aws_secret'.") }}
+  {% endif %}
+  {% if manifest %}
+  MANIFEST
+  {% endif %}
+  DELIMITER AS '{{ delimiter }}'
+  NULL AS '{{ null_as }}'
+  MAXFILESIZE AS {{ max_file_size }}
+  {% if escape %}
+  ESCAPE
+  {% endif %}
+  {% if compression %}
+  {{ compression|upper }}
+  {% endif %}
+  {% if add_quotes %}
+  ADDQUOTES
+  {% endif %}
+  {% if encrypted %}
+  ENCRYPTED
+  {% endif %}
+  {% if overwrite %}
+  ALLOWOVERWRITE
+  {% endif %}
+  {% if not parallel %}
+  PARALLEL OFF
+  {% endif %}
+
+{% endmacro %}
